df0312f44941e9341e6efd28a4adfe86e16f1ea4
devops710 <toon710@gmail.com>
2024-02-21 14:44:00 +0900

    2402211444

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          pray-project
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="http://localhost:3000/main"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          main
         </a>
       </header>
     </div>
