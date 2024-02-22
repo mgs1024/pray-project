79e2f413f0a5636ba625f93dabb122d2eb63eadb
mgs <hu911024@gmail.com>
2024-02-22 17:02:56 +0900

    layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import TourList from "./ui/components/TourList";
+import Main from "./ui/components/Main";
+import ChargingMap from "./ui/components/ChargingMap";
+import Favorite from "./ui/components/Favorite";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path={"/"} element={<Main />} />
+        <Route path={"/tour"} element={<TourList />} />
+        <Route path={"/map"} element={<ChargingMap />} />
+        <Route path={"/favorite"} element={<Favorite />} />
+      </Routes>
     </div>
   );
 }
