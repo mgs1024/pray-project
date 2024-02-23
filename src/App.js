718ea8f47a0b40c7f9f637ad61fa45035004a8c5
mgs <hu911024@gmail.com>
2024-02-23 18:12:51 +0900

    20240223 layout/tour

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import "./App.css";
-import { Route, Routes } from "react-router-dom";
-import TourList from "./ui/components/TourList";
-import Main from "./ui/components/Main";
-import ChargingMap from "./ui/components/ChargingMap";
-import Favorite from "./ui/components/Favorite";
+import './App.css';
+import {Route, Routes} from 'react-router-dom';
+import TourList from './ui/components/TourList';
+import Main from './ui/components/Main';
+import ChargingMap from './ui/components/ChargingMap';
+import Favorite from './ui/components/Favorite';
+import TourInfo from './ui/components/TourInfo';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path={"/"} element={<Main />} />
-        <Route path={"/tour"} element={<TourList />} />
-        <Route path={"/map"} element={<ChargingMap />} />
-        <Route path={"/favorite"} element={<Favorite />} />
+        <Route path={'/'} element={<Main />} />
+        <Route path={'/tour'} element={<TourList />} />
+        <Route path={'/map'} element={<ChargingMap />} />
+        <Route path={'/favorite'} element={<Favorite />} />
+        <Route path={'/info'} element={<TourInfo />} />
       </Routes>
     </div>
   );
