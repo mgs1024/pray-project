a6e3f9812e7be75e88344a3a5f98863a4a77bbb4
devops710 <toon710@gmail.com>
2024-02-26 17:58:42 +0900

    2402261758

--- src/ui/components/ChargingMap.js
+++ src/ui/components/ChargingMap.js
@@ -3,43 +3,51 @@ import {useLocation, useNavigate} from 'react-router-dom';
 
 const ChargingMap = () => {
   const ref = useRef(null);
-  const [mapLong, setMapLong] = useState(null);
-  const [mapLat, setMapLat] = useState(null);
+  //const [mapLong, setMapLong] = useState(null);
+  //const [mapLat, setMapLat] = useState(null);
+  const [marker, setMarker] = useState(null);
   const location = useLocation();
   const navigate = useNavigate();
   const mapApiKey = process.env.REACT_APP_MAP_API_KEY;
 
   useEffect(() => {
-    if (!window.google) {
-      const script = document.createElement('script');
-      script.src = `https://maps.googleapis.com/maps/api/js?key=${mapApiKey}&callback=initMap&loading=async`;
-      script.async = true;
-      script.defer = true;
-    
-      window.initMap = () => {
-        const mapOptions = {
-          center: { lat: parseFloat(location.state.mapLat), lng: parseFloat(location.state.mapLong) },
-          zoom: 10,
-        };
-    
-        const map = new window.google.maps.Map(ref.current, mapOptions);
+    const script = document.createElement('script');
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${mapApiKey}&callback=initMap&loading=async`;
+    script.async = true;
+    script.defer = true;
+  
+    window.initMap = () => {
+      const mapOptions = {
+        center: { lat: parseFloat(location.state.mapLat), lng: parseFloat(location.state.mapLong) },
+        zoom: 16,
       };
-    
-      document.head.appendChild(script);
-    
-      return () => {
-        document.head.removeChild(script);
-        delete window.initMap;
-      };
-    }
-  }, []);
+  
+      const map = new window.google.maps.Map(ref.current, mapOptions);
+
+      const pin = new window.google.maps.Marker({
+        position: { lat: parseFloat(location.state.mapLat), lng: parseFloat(location.state.mapLong) },
+        map: map,
+        title: '요기요',
+      });
+
+      setMarker(pin);
+
+    };
+  
+    document.head.appendChild(script);
+  
+    return () => {
+      document.head.removeChild(script);
+      delete window.initMap;
+    };
+  }, [location.state]);
   
 
   const onClick = e => {
     const long = e.currentTarget.getAttribute('data-long');
     const lat = e.currentTarget.getAttribute('data-lat');
-    setMapLong(long);
-    setMapLat(lat);
+    //setMapLong(long);
+    //setMapLat(lat);
     console.log(long, lat);
     navigate('/tour', {state: {tourLong: long, tourLat: lat}});
   }; 
