79f7e91a0687210251aba01505a5b8117e7d2610
mgs <hu911024@gmail.com>
2024-03-05 14:23:11 +0900

    20240305 tour map

--- src/App.js
+++ src/App.js
@@ -5,17 +5,28 @@ import Main from './ui/components/Main';
 import ChargingMap from './ui/components/ChargingMap';
 import Favorite from './ui/components/Favorite';
 import TourInfo from './ui/components/TourInfo';
+import TourMap from './ui/components/TourMap';
+import {createContext} from 'react';
+export const AppContext = createContext();
 
 function App() {
+  const coordi = {
+    csNm: '',
+    long: '',
+    lat: '',
+  };
   return (
     <div className="App">
-      <Routes>
-        <Route path={'/'} element={<Main />} />
-        <Route path={'/tour'} element={<TourList />} />
-        <Route path={'/map'} element={<ChargingMap />} />
-        <Route path={'/favorite'} element={<Favorite />} />
-        <Route path={'/info'} element={<TourInfo />} />
-      </Routes>
+      <AppContext.Provider value={coordi}>
+        <Routes>
+          <Route path={'/'} element={<Main />} />
+          <Route path={'/tour'} element={<TourList />} />
+          <Route path={'/map'} element={<ChargingMap />} />
+          <Route path={'/favorite'} element={<Favorite />} />
+          <Route path={'/info'} element={<TourInfo />} />
+          <Route path={'/tourMap'} element={<TourMap />} />
+        </Routes>
+      </AppContext.Provider>
     </div>
   );
 }
